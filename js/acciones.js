1191f146d30e82d46c3529ff000b3f2e3ecc2e83
canetefortozo <karla3f.dsaupoo@gmail.com>
2016-05-12 13:35:20 -0500

    2subida
    aplicacion08

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener('deviceready', function() {
 		$('#beep').tap(function(){
-			navigator.notification.brrp(1);//numero de veces que deseas que notifique
+			navigator.notification.beep(1);//numero de veces que deseas que notifique
 		});//beep
 		$('#btnvibrar').on('tap', function () {
 			navigator.notification.vibrate(2000);
